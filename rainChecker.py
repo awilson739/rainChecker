456840dcabefcfc03267175726ef1c5d77606102
Angela <angela.wilson739@gmail.com>
2016-09-28 21:22:40 -0400

    Added twilio functionality

--- rainChecker.py
+++ rainChecker.py
@@ -1,10 +1,16 @@
-#!/usr/local/bin/python3.5
+#!/usr/bin/python3
 import json
 import urllib.request
-api = 'http://api.aerisapi.com/forecasts/arlington,va?from=today&to=today&client_id=&client_secret='
+from twilio.rest import TwilioRestClient
+accountSID=''
+authToken=''
+api = 'http://api.aerisapi.com/forecasts/arlington,va?from=today&to=today&client_id=client_secret='
 response = urllib.request.urlopen(api)
 str_response = response.read().decode('utf-8')
 w = json.loads(str_response)
+twilioCli=TwilioRestClient(accountSID,authToken)
+myTwilioNumber=''
+myNumber=''
 #print(str_response)
 #print(w)
 #print(ob['periods'])
@@ -13,3 +19,5 @@ day_weather = ob['periods']
 #print(type(day_weather))
 for element in day_weather:
    print(element['weather'])
+   message = twilioCli.messages.create(body=element['weather'], from_=myTwilioNumber, to=myNumber)
+
