9a8c5487f89d2cbf57abfe5bdaf49bef6da0053e
Angela <angela.wilson739@gmail.com>
2016-09-29 16:58:13 -0400

    Add files via upload
    Added functions, config, regex. Need to test when a regex returns None so it won't send a message.

--- rainChecker.py
+++ rainChecker.py
@@ -1,23 +1,30 @@
-#!/usr/bin/python3
+#!virt/bin/python3
 import json
 import urllib.request
+import config
+import re
 from twilio.rest import TwilioRestClient
-accountSID=''
-authToken=''
-api = 'http://api.aerisapi.com/forecasts/arlington,va?from=today&to=today&client_id=client_secret='
-response = urllib.request.urlopen(api)
-str_response = response.read().decode('utf-8')
-w = json.loads(str_response)
-twilioCli=TwilioRestClient(accountSID,authToken)
-myTwilioNumber=''
-myNumber=''
-#print(str_response)
-#print(w)
-#print(ob['periods'])
-ob = w['response'][0]
-day_weather = ob['periods']
-#print(type(day_weather))
-for element in day_weather:
-   print(element['weather'])
-   message = twilioCli.messages.create(body=element['weather'], from_=myTwilioNumber, to=myNumber)
+def get_weather(city):
+   api = 'http://api.aerisapi.com/forecasts/%s?from=today&to=today&client_id=%s&client_secret=%s' %(city,config.AERIS_ID,config.AERIS_SECRET)
+   response = urllib.request.urlopen(api)
+   str_response = response.read().decode('utf-8')
+   js = json.loads(str_response)
+   ob = js['response'][0]
+   day_weather=ob['periods']
+   return day_weather
+
+def check_rain(day_weather):
+   rainregex = re.compile(r'shower(s)',re.I)
+   sunnyregex = re.compile(r'sunny',re.I)
+   for element in day_weather:
+      if sunnyregex.search(element['weather']):
+         return element['weather'] 
+
+def send_message(weather):
+   twilioCli=TwilioRestClient(config.SID,config.AUTH_TOKEN)
+   if weather != 'None':
+      message = twilioCli.messages.create(body=weather, from_=config.twiNumber, to=config.myNumber)
+
+w = get_weather('arlington,va')
+send_message(check_rain(w))
 
