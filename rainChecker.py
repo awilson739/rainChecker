f9bca9936898e12b78a7c7b977a4d8b4cff48c17
Angela <angela.wilson739@gmail.com>
2016-09-29 22:25:28 -0400

    Fixed behavior when it returns NoneType. Need to make it Pep8 compliant.

--- rainChecker.py
+++ rainChecker.py
@@ -1,4 +1,4 @@
-#!virt/bin/python3
+#!/usr/bin/python3
 import json
 import urllib.request
 import config
@@ -17,14 +17,13 @@ def check_rain(day_weather):
    rainregex = re.compile(r'shower(s)',re.I)
    sunnyregex = re.compile(r'sunny',re.I)
    for element in day_weather:
-      if sunnyregex.search(element['weather']):
+      if rainregex.search(element['weather']):
          return element['weather'] 
 
 def send_message(weather):
    twilioCli=TwilioRestClient(config.SID,config.AUTH_TOKEN)
-   if weather != 'None':
+   if weather != None:
       message = twilioCli.messages.create(body=weather, from_=config.twiNumber, to=config.myNumber)
 
 w = get_weather('arlington,va')
 send_message(check_rain(w))
-
